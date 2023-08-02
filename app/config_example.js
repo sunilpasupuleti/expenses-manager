1e9c96b07f336025de38327b3fc423b9657c5607
Sunil Kumar Pasupuleti <sunilkumarpasupuleti@Sunils-MacBook-Air.local>
2023-08-02 10:58:49 +0530

    Delete confidential files

--- app/config_example.js
+++ app/config_example.js
@@ -1,6 +1,17 @@
-export const BACKEND_URL =
-  'http://<your backend url where u have hosted or started>';
+export const BACKEND_URL ='http://<your backend url where u have hosted or started>';
 export const WEB_CLIENT_ID = '<Web client id from Firebase console>';
-export const GOOGLE_API_KEY = 'Google Api Key from Google Cloud Platform';
-export const GOOGLE_CLOUD_VISION_API_URL =
-  'https://vision.googleapis.com/v1/images:annotate';
+export const GOOGLE_API_KEY = 'Credentials/API we have created in the installation step from Google Cloud Platform';
+export const GOOGLE_CLOUD_VISION_API_URL ='https://vision.googleapis.com/v1/images:annotate';
+export const BACKEND_URL = 'https://ec2.webwizard.in:3000';
+export const WEB_CLIENT_ID =
+ '4544693826-dcfvcng0op5c5me3homei0mcki4k6ue9.apps.googleusercontent.com';
+export const MINDEE_API_URL =
+ 'https://api.mindee.net/v1/products/mindee/expense_receipts/v5/predict' (Smart scan receipt using MINDEE);
+export const MINDEE_API_KEY = MIndee api key here;
+export const ONESIGNAL_APP_ID = One signal app id to register;
+export const ACCOUNT_DELETION_URL =
+ 'https://expenses-manager.webwizard.in/#/account-deletion';
+export const APP_STORE_URL =
+ 'https://apps.apple.com/us/app/expenses-manager-by-webwizard/id6450874945';
+export const PLAY_STORE_URL =
+ 'https://play.google.com/store/apps/details?id=com.webwizard.expensesmanager';
